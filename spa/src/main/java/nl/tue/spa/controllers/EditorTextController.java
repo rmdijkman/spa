5bd9314c788b9139aa1fb43a81160e448ef969d6
rdijkman <r.m.dijkman@tue.nl>
2016-01-21 11:14:20 +0100

    Fixed bug editor 'undo' does not mark the file as changed on Mac.

--- spa/src/main/java/nl/tue/spa/controllers/EditorTextController.java
+++ spa/src/main/java/nl/tue/spa/controllers/EditorTextController.java
@@ -86,11 +86,6 @@ public class EditorTextController extends EditorController implements KeyListene
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		if (saved){
-			saved = false;
-			Environment.getEditorContainerController().updateSavedState();
-		}
-		
 	}
 
 	@Override
@@ -108,9 +103,13 @@ public class EditorTextController extends EditorController implements KeyListene
 			}
 		}
 		if ((e.getKeyCode() == KeyEvent.VK_Z) && (e.isMetaDown() || e.isControlDown())){
-			gui.undo();
+			gui.undo();			
 		}else if ((e.getKeyCode() == KeyEvent.VK_Y) && (e.isMetaDown() || e.isControlDown())){
 			gui.redo();
+		}
+		if (saved){
+			saved = false;
+			Environment.getEditorContainerController().updateSavedState();
 		}
 	}
 
