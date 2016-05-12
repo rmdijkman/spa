9ae6618ab021589152ec6407e8a5f14d90f0a835
rdijkman <r.m.dijkman@tue.nl>
2016-05-12 17:41:30 +0200

    Found the way to correctly set the library location in R.

--- spa/src/test/java/RTest.java
+++ spa/src/test/java/RTest.java
@@ -3,6 +3,7 @@ import nl.tue.spa.executor.r.RProcessor;
 /**
  *	To use R, the environment variables must be set correctly:
  *	R_HOME must be set (e.g. C:/Program Files/R/R-3.1.2)
+ *  R_LIBS_USER may need to be set, if it does not point to the location where the R libraries are installed (e.g. G:/R/win-library/3.1)
  *	PATH must be set to include:
  *  - the folder that contains the appropriate binary file for running R, e.g. 
  *  	C:/Program Files/R/R-3.1.2/bin/x64)
@@ -12,11 +13,13 @@ import nl.tue.spa.executor.r.RProcessor;
  *  	/Library/Frameworks/R.framework/Resources/library/rJava/jri
  *    You can set this as an environment variable, e.g.:
  *    -Djava.library.path=/Library/Frameworks/R.framework/Resources/library/rJava/jri
- *  This obviously requires that R is installed. 
+ *    
+ *  This requires that R is installed.
+ *   
  *  JRI must also be installed. This can be done by running the following command inside of R:
  *  install.packages("rJava")  
  *
- *	Also requires jsonlite:
+ *	jsonlite must also be installed:
  *	install.packages("jsonlite")
  *
  */
